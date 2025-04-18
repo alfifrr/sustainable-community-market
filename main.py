aa74472c6ae92362bd305407ad959bde36e65c3b
istamosh <63596969+istamosh@users.noreply.github.com>
2025-04-19 01:08:00 +0700

    pre-deploy

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from app import create_app
 from app.apis import blueprints
+from flask_swagger_ui import get_swaggerui_blueprint
+
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.json'
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Sustainable Community Market API"
+    }
+)
 
 app = create_app()
+app.register_blueprint(swagger_ui_blueprint)
 for blueprint, url_prefix in blueprints:
     app.register_blueprint(blueprint, url_prefix=url_prefix)
 
