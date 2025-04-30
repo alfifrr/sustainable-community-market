9f4a070a2daec087d18a2cbc2faa2b5467062947
istamosh <63596969+istamosh@users.noreply.github.com>
2025-04-30 15:18:06 +0700

    fix: product creation expiry date format

--- app/utils/validators/product.py
+++ app/utils/validators/product.py
@@ -8,53 +8,73 @@ from flask_jwt_extended import get_jwt_identity
 
 
 class ProductForm(BaseForm):
-    name = StringField('Name', validators=[
-        DataRequired(message='Product name is required'),
-        Length(min=3, max=255,
-               message='Product name must be between 3 and 255 characters')
-    ])
-    description = StringField('Description', validators=[
-        DataRequired(message='Product description is required'),
-        Length(min=10, max=255,
-               message='Description must be between 10 and 255 characters')
-    ])
-    price = IntegerField('Price', validators=[
-        DataRequired(message='Product price is required'),
-        NumberRange(min=1, message='Price cannot below zero')
-    ])
-    stock = IntegerField('Stock', validators=[
-        DataRequired(message='Product stock is required'),
-        NumberRange(min=1, message='Stock cannot below zero')
-    ])
-    category_id = IntegerField('Category', validators=[
-        DataRequired(message='Category ID is required')
-    ])
-    address_id = IntegerField('Address', validators=[
-        DataRequired(message='Address ID is required')
-    ])
+    name = StringField(
+        "Name",
+        validators=[
+            DataRequired(message="Product name is required"),
+            Length(
+                min=3,
+                max=255,
+                message="Product name must be between 3 and 255 characters",
+            ),
+        ],
+    )
+    description = StringField(
+        "Description",
+        validators=[
+            DataRequired(message="Product description is required"),
+            Length(
+                min=10,
+                max=255,
+                message="Description must be between 10 and 255 characters",
+            ),
+        ],
+    )
+    price = IntegerField(
+        "Price",
+        validators=[
+            DataRequired(message="Product price is required"),
+            NumberRange(min=1, message="Price cannot below zero"),
+        ],
+    )
+    stock = IntegerField(
+        "Stock",
+        validators=[
+            DataRequired(message="Product stock is required"),
+            NumberRange(min=1, message="Stock cannot below zero"),
+        ],
+    )
+    category_id = IntegerField(
+        "Category", validators=[DataRequired(message="Category ID is required")]
+    )
+    address_id = IntegerField(
+        "Address", validators=[DataRequired(message="Address ID is required")]
+    )
     expiration_date = DateTimeField(
-        'Expiration Date',
+        "Expiration Date",
         validators=[
             DataRequired(
-                message='Expiration date is required (example: 2000-11-22T23:59:59Z)')
+                message="Expiration date is required (example: 2026-11-22T23:59:59.359Z)"
+            )
         ],
-        format='%Y-%m-%dT%H:%M:%SZ')  # ISO 8601 format
+        format="%Y-%m-%dT%H:%M:%S.%fZ",
+    )  # ISO 8601 format with milliseconds
 
     def validate_category_id(self, field):
         category = Category.query.get(field.data)
         if not category:
-            raise ValidationError('Category does not exist')
+            raise ValidationError("Category does not exist")
 
     def validate_address_id(self, field):
         current_user_id = get_jwt_identity()
         address = Address.query.filter_by(
-            user_id=current_user_id,
-            id=field.data).first()
+            user_id=current_user_id, id=field.data
+        ).first()
         if not address:
-            raise ValidationError('Address does not exist')
+            raise ValidationError("Address does not exist")
 
     def validate_expiration_date(self, field):
         expiration_date = field.data.replace(tzinfo=timezone.utc)
         current_time = datetime.now(timezone.utc)
         if expiration_date <= current_time:
-            raise ValidationError('Expiration date must be in the future')
+            raise ValidationError("Expiration date must be in the future")
